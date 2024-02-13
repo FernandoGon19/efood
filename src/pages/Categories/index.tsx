a2c700241d74e010fc6e9cbbffae580de2d5e274
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2024-02-13 12:41:16 -0300

    cardapio

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -5,9 +5,34 @@ import ProdutoCategoria from '../../components/ProdutosCategorie'
 import pizza from '../../components/assets/PizzaCategoria.png'
 import { useEffect, useState } from 'react'
 
+type Cardapio = {
+  foto: string
+  preco: number
+  id: number
+  nome: string
+  descricao: string
+  porcao: string
+}
+
 const Categories = () => {
+  const [cardapio, setCardapio] = useState<Cardapio[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes').then(
+      (res) => res.json().then((res) => setCardapio(res))
+    )
+  })
+
+  console.log(cardapio)
+
   return (
     <div>
+      {cardapio.map((pratos) => (
+        <div key={pratos.id}>
+          <h1>{pratos.nome}</h1>
+        </div>
+      ))}
+
       <HeaderCategorie></HeaderCategorie>
       <Banner></Banner>
       <div className="container grid">
